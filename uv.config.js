ad1a77182e7496a101bb5e5530e08f317970c2fb
classroom84x-games <Classroom84X@outlook.com>
2025-06-29 19:54:19 -0400

    Update bare server
    *Warning* This server is hosted by Vercel and may not be compatible with some filters that have Vercel links blocked *Warning*

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/science/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://ojojojojojoj.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv.handler.js',
