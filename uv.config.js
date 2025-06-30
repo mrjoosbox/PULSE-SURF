11400fa4973fd67df522ce438314785ad815dd89
mrjoosbox <149395873+mrjoosbox@users.noreply.github.com>
2025-06-30 03:12:56 -0600

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/science/',
-    bare: 'http://5.161.232.18:8080/bare',
+    bare: 'http://5.161.232.18:8080/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv.handler.js',
