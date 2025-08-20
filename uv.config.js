a5ac1a41c6eba3ddaa758b04cad6f36bd96c0a42
classroom84x-games <Classroom84X@outlook.com>
2025-08-19 23:55:09 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/science/',
-    bare: 'ojojojojojoj.vercel.com/bare/'
+    bare: 'https://c84x-bare-server.vercel.app/bare/'
     ,encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: 'uv.handler.js',
